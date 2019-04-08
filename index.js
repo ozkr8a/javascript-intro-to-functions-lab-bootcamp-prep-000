32d3f59e73e5b4da10aa10391cd1c3f7d23d0be1
Oscar <oscar_ochoac@yahoo.com>
2019-04-08 19:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var uppercase, lowercase,  = string
+  var uppercase, lowercase, ILU = string
       lowercase = "hello!"
       ILU = "I Love you, Grandma."
   
